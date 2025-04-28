7663c590a7e625c3da8307ede512c5e298957b28
deletebackspace <hoanpv076@gmail.com>
2025-04-28 22:07:10 +0700

    Tạo các list danh sách sản phẩm

--- playvault/src/pages/Home.jsx
+++ playvault/src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Slideshow from '../components/home/slideshow/slideshow.jsx';
+import ProductCategories from '../components/home/product/ProductCategories.jsx';
+import ProductList from '../components/home/product/ProductList.jsx';
+import ProductListNgang from '../components/home/product/ProductListNgang.jsx';
 
 const Home = () => {
     return (
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center p-0 w-full h-full">
-            <Slideshow />
-        </div>
+        <>
+            <div className="min-h-screen bg-gray-100 flex items-center justify-center p-0 w-full h-full">
+                <Slideshow />
+            </div>
+            <ProductCategories />
+
+        </>
+
+
     );
 };
 
